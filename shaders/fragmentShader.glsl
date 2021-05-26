ea2f9814354c346d4506416d2c12279c27bb2bdf
Alessandra Boccuto <boccuto.alessandra@gmail.com>
2021-05-26 17:24:35 +0200

    modified fragment and vertex shaders

--- shaders/fragmentShader.glsl
+++ shaders/fragmentShader.glsl
@@ -1,12 +1,34 @@
-  precision mediump float;
+precision highp float;
 
-  varying vec3 v_normal;
+varying vec3 v_normal;
+varying vec3 v_surfaceToView;
+varying vec4 v_color;
 
-  uniform vec4 u_diffuse;
-  uniform vec3 u_lightDirection;
+uniform vec3 diffuse;
+uniform vec3 ambient;
+uniform vec3 emissive;
+uniform vec3 specular;
+uniform float shininess;
+uniform float opacity;
+uniform vec3 u_lightDirection;
+uniform vec3 u_ambientLight;
 
-  void main () {
-    vec3 normal = normalize(v_normal);
-    float fakeLight = dot(u_lightDirection, normal) * .5 + .5;
-    gl_FragColor = vec4(u_diffuse.rgb * fakeLight, u_diffuse.a);
-  }
+void main () {
+  vec3 normal = normalize(v_normal);
+
+  vec3 surfaceToViewDirection = normalize(v_surfaceToView);
+  vec3 halfVector = normalize(u_lightDirection + surfaceToViewDirection);
+
+  float fakeLight = dot(u_lightDirection, normal) * .5 + .5;
+  float specularLight = clamp(dot(normal, halfVector), 0.0, 1.0);
+
+  vec3 effectiveDiffuse = diffuse * v_color.rgb;
+  float effectiveOpacity = opacity * v_color.a;
+
+  gl_FragColor = vec4(
+      emissive +
+      ambient * u_ambientLight +
+      effectiveDiffuse * fakeLight +
+      specular * pow(specularLight, shininess),
+      effectiveOpacity);
+}
--- shaders/vertexShader.glsl
+++ shaders/vertexShader.glsl
@@ -1,13 +1,20 @@
 attribute vec4 a_position;
 attribute vec3 a_normal;
+attribute vec4 a_color;
 
 uniform mat4 u_projection;
 uniform mat4 u_view;
 uniform mat4 u_world;
+uniform vec3 u_viewWorldPosition;
 
 varying vec3 v_normal;
+varying vec3 v_surfaceToView;
+varying vec4 v_color;
 
 void main() {
-    gl_Position = u_projection * u_view * u_world * a_position;
+    vec4 worldPosition = u_world * a_position;
+    gl_Position = u_projection * u_view * worldPosition;
+    v_surfaceToView = u_viewWorldPosition - worldPosition.xyz;
     v_normal = mat3(u_world) * a_normal;
+    v_color = a_color;
 }
