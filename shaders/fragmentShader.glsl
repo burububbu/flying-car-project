cb4cb60bf0cc920303581618b60d6b6ef964c73d
Alessandra Boccuto <boccuto.alessandra@gmail.com>
2021-06-04 12:49:26 +0200

    modify shaders to handle bump mapping

--- shaders/fragmentShader.glsl
+++ shaders/fragmentShader.glsl
@@ -1,15 +1,18 @@
 precision highp float;
 
   varying vec3 v_normal;
+  varying vec3 v_tangent;
   varying vec3 v_surfaceToView;
   varying vec2 v_texcoord;
   varying vec4 v_color;
 
   uniform vec3 diffuse;
   uniform sampler2D diffuseMap;
+  uniform sampler2D normalMap;
   uniform vec3 ambient;
   uniform vec3 emissive;
   uniform vec3 specular;
+  uniform sampler2D specularMap;
   uniform float shininess;
   uniform float opacity;
   uniform vec3 u_lightDirection;
@@ -18,11 +21,20 @@ precision highp float;
   void main () {
     vec3 normal = normalize(v_normal);
 
+    vec3 tangent = normalize(v_tangent);
+    vec3 bitangent = normalize(cross(normal, tangent));
+ 
+    mat3 tbn = mat3(tangent, bitangent, normal);
+    normal = texture2D(normalMap, v_texcoord).rgb * 2. - 1.;
+    normal = normalize(tbn * normal);
+
     vec3 surfaceToViewDirection = normalize(v_surfaceToView);
     vec3 halfVector = normalize(u_lightDirection + surfaceToViewDirection);
 
     float fakeLight = dot(u_lightDirection, normal) * .5 + .5;
     float specularLight = clamp(dot(normal, halfVector), 0.0, 1.0);
+    vec4 specularMapColor = texture2D(specularMap, v_texcoord);
+    vec3 effectiveSpecular = specular * specularMapColor.rgb;
 
     vec4 diffuseMapColor = texture2D(diffuseMap, v_texcoord);
     vec3 effectiveDiffuse = diffuse * diffuseMapColor.rgb * v_color.rgb;
@@ -32,6 +44,7 @@ precision highp float;
         emissive +
         ambient * u_ambientLight +
         effectiveDiffuse * fakeLight +
-        specular * pow(specularLight, shininess),
+        // specular * pow(specularLight, shininess),
+        effectiveSpecular * pow(specularLight, shininess),
         effectiveOpacity);
   }
